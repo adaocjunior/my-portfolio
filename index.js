4b946a091bc40ba7634856aabd5fbb3d6c555ab4
adaocjunior <adao8678@gmail.com>
2022-04-29 17:25:10 -0300

    Porfolio v1

---
+++ index.js
@@ -0,0 +1,29 @@
+/*const body = document.getElementById('body')
+const pagelogo = document.getElementById('pageLogo')
+const headerBackground = document.getElementById('backgroundStars')
+const header = document.getElementById('header')
+const footer = document.getElementById('footer')
+const footerStars = document.getElementById('footerStars')
+const titles = document.getElementsByClassName('title')
+function themeChange(bool){
+    theme = bool
+    switch (theme){
+        case false:
+            break
+        case true:
+            pagelogo.src= "./images/portfolioShell.png"
+            headerBackground.src ="./images/headerSand.png"
+            footerStars.src = "./images/footerSand.png"
+            header.style.backgroundColor = "#0BC9CD"
+            footer.style.backgroundColor = "#0BC9CD"
+            body.style.backgroundColor = "#1D2F6F"
+            
+            break
+    }
+}
+*/
+
+const rpsGameVideo = document.getElementById('videoRPSGame')
+const alarmClockVideo = document.getElementById('videoAlarmClock')
+
+alarmClockVideo.addEventListener('play', (onmouseenter) => {})
